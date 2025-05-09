2c6eecec09071096ca513ac4bc8d4225a71c69d4
Yujie-test <18311345153@163.com>
2025-05-09 21:41:30 +0800

    a

--- Spark/src/main/java/spark/rdd/operate/Spark02_Operate_Transform_Map.java
+++ Spark/src/main/java/spark/rdd/operate/Spark02_Operate_Transform_Map.java
@@ -1,7 +1,12 @@
 package spark.rdd.operate;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Spark02_Operate_Transform_Map {
     public static void main(String[] args) {
@@ -11,6 +16,29 @@ public class Spark02_Operate_Transform_Map {
         conf.setAppName("spark");
 
         final JavaSparkContext jsc = new JavaSparkContext(conf);
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4);
+
+        // TODO RDD的方法
+        JavaRDD<Integer> rdd = jsc.parallelize(
+                nums, 2
+        );
+
+        // TODO RDD的转换方法：Map
+        //  对单值数据进行处理
+
+        // map；映射（K —> V），将A(K)转换为B(V)
+        //      将指定的值转换为其他值的场合
+        //      [1, 2, 3, 4] -> [2, 4, 6, 8]
+        //      学习方法重点：1. 名字 2. IN 3. OUT
+        JavaRDD<Object> newRDD = rdd.map(new Function<Integer, Object>() {
+            @Override
+            public Object call(Integer in) throws Exception {
+                return in * 2;
+            }
+        });
+        newRDD.collect().forEach(System.out::println);
+        System.out.println("****************************************");
+        System.out.println(nums);
 
         // TODO RDD的方法
 
